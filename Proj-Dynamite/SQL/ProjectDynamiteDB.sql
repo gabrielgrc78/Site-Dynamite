47d65b5b5a0900d88ded7698886fc9aada30d40b
Gabriel Garcia <gabrielgrc78@gmail.com>
2019-06-19 14:00:01 -0400

    updating the .html and working on the SQL file

--- Proj-Dynamite/SQL/ProjectDynamiteDB.sql
+++ Proj-Dynamite/SQL/ProjectDynamiteDB.sql
@@ -1,18 +1,20 @@
 DROP DATABASE IF EXISTS ProjectDynamite;
 CREATE DATABASE ProjectDynamite;
 
-DROP TABLE IF EXISTS ProjectDynamite.FacultyAccounts;
-CREATE TABLE ProjectDynamite.FacultyAccounts(
+DROP TABLE IF EXISTS ProjectDynamite.ExecutiveAccounts;
+CREATE TABLE ProjectDynamite.ExecutiveAccounts(
     id       INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     username TINYTEXT                           NOT NULL,
     email    TINYTEXT                           NOT NULL,
-    password LONGTEXT                           NOT NULL
-) ENGINE= InnoDB AUTO_INCREMENT = 1 DEFAULT;
+    password LONGTEXT                           NOT NULL,
+    role     TINYTEXT                           NOT NULL
+)ENGINE= InnoDB AUTO_INCREMENT=1 DEFAULT;
 
 DROP TABLE IF EXISTS ProjectDynamite.StudentAccount;
 CREATE TABLE ProjectDynamite.StudentAccount(
   id        int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   Username  VARCHAR(50)                        NOT NULL,
   email     VARCHAR(50)                        NOT NULL,
-  password  LONGTEXT                           NOT NULL
+  password  LONGTEXT                           NOT NULL,
+  classes   TINYTEXT                           NOT NULL
 )ENGINE= InnoDB AUTO_INCREMENT=1 DEFAULT;
