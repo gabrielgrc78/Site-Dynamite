1a082dfd40672ccb2452f401d4026876905b609a
Gabriel Garcia <gabrielgrc78@gmail.com>
2019-06-22 02:15:09 -0400

    updating SQL

--- Proj-Dynamite/SQL/ProjectDynamiteDB.sql
+++ Proj-Dynamite/SQL/ProjectDynamiteDB.sql
@@ -3,18 +3,30 @@ CREATE DATABASE ProjectDynamite;
 
 DROP TABLE IF EXISTS ProjectDynamite.ExecutiveAccounts;
 CREATE TABLE ProjectDynamite.ExecutiveAccounts(
-    id       INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    username TINYTEXT                           NOT NULL,
-    email    TINYTEXT                           NOT NULL,
-    password LONGTEXT                           NOT NULL,
-    role     TINYTEXT                           NOT NULL
-)ENGINE= InnoDB AUTO_INCREMENT=1 DEFAULT;
+    `ExecId`   INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `username` VARCHAR(50)                        NOT NULL,
+    `email`    VARCHAR(50)                        NOT NULL,
+    `password` LONGTEXT                           NOT NULL,
+    `role`     TINYTEXT                           NOT NULL,
+	UNIQUE KEY `ExecName`(`username`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 ;
 
 DROP TABLE IF EXISTS ProjectDynamite.StudentAccount;
 CREATE TABLE ProjectDynamite.StudentAccount(
-  id        int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  Username  VARCHAR(50)                        NOT NULL,
-  email     VARCHAR(50)                        NOT NULL,
-  password  LONGTEXT                           NOT NULL,
-  classes   TINYTEXT                           NOT NULL
-)ENGINE= InnoDB AUTO_INCREMENT=1 DEFAULT;
+  `StudentId` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `Username`  VARCHAR(50)                        NOT NULL,
+  `email`     VARCHAR(50)                        NOT NULL,
+  `password`  LONGTEXT                           NOT NULL,
+  `classes`   TINYTEXT                           NOT NULL
+
+)ENGINE=InnoDB AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS ProjectDynamite.SecurityLogs;
+CREATE TABLE ProjectDynamite.SecurityLogs(
+  `Student`       VARCHAR(50)                   NOT NULL,
+  `Executives`    VARCHAR(50)                   NOT NULL,
+  `lastlogin`     TIMESTAMP   NULL     DEFAULT  NULL,
+  `CreatedDate`   TIMESTAMP   NOT NULL DEFAULT  CURRENT_TIMESTAMP,
+  KEY `SecurityLogs_ibfk_1`(`Student`),
+  FOREIGN KEY (`Student`) REFERENCES `StudentAccount`(`StudentId`) ON DELETE CASCADE
+)ENGINE=InnoDB;
