766ccb304847c04b81cd08928b9cf1d91c95ac81
Gabriel Garcia <gabrielgrc78@gmail.com>
2019-07-03 08:54:52 -0400

    modified:   Proj-Dynamite/SQL/ProjectDynamiteDB.sql

--- Proj-Dynamite/SQL/ProjectDynamiteDB.sql
+++ Proj-Dynamite/SQL/ProjectDynamiteDB.sql
@@ -4,20 +4,41 @@ CREATE DATABASE ProjectDynamite;
 DROP TABLE IF EXISTS ProjectDynamite.UserAccounts;
 CREATE TABLE ProjectDynamite.UserAccounts(
     `UserId`           INT(11)      AUTO_INCREMENT  PRIMARY KEY NOT NULL,
-    `username`         VARCHAR(50)                              NOT NULL,
-    `password`         LONGTEXT                            	    NOT NULL,
+    `Username`         VARCHAR(50)                              NOT NULL,
+    `Password`         LONGTEXT                            	    NOT NULL,
     `Userlevel`        TINYTEXT                           	    NOT NULL,
-    `classes`          TINYTEXT                           	    NULL,
 	`CreatedDate`      TIMESTAMP    NOT NULL 	    DEFAULT     CURRENT_TIMESTAMP,
-     UNIQUE (`username`)
+     UNIQUE (`Username`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 ;
 
 DROP TABLE IF EXISTS ProjectDynamite.SecurityLogs;
 CREATE TABLE ProjectDynamite.SecurityLogs (
     `SecId`   	INT(11)   	AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    `username`  VARCHAR(50) 					       NOT NULL,
-    `lastlogin` TIMESTAMP 	NULL 		       DEFAULT     NULL,
-    CONSTRAINT `securitylogs_ibfk_1` FOREIGN KEY (`username`)
-        REFERENCES `UserAccounts` (`username`)
+    `Username`  VARCHAR(50) 					       NOT NULL,
+    `lastlogin` TIMESTAMP 	NULL 		   DEFAULT     NULL,
+    CONSTRAINT `securitylogs_ibfk_1` FOREIGN KEY (`Username`)
+        REFERENCES `UserAccounts` (`Username`)
         ON DELETE CASCADE 
 )ENGINE=INNODB AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS projectdynamite.ClassRoster;
+CREATE TABLE projectdynamite.ClassRoster (
+`ClassCode` INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+`ClassName` VARCHAR(50) NOT NULL,
+`RoomNumber` TINYTEXT NOT NULL,
+`Description` LONGTEXT NOT NULL,
+UNIQUE(`ClassCode`),
+UNIQUE(`ClassName`)
+)ENGINE=INNODB AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS projectdynamite.StudentEnrollment;
+CREATE TABLE projectdynamite.StudentEnrollment (
+`Username` VARCHAR(50) NOT NULL,
+`ClassCode` INT(11) NOT NULL,
+`ClassName` VARCHAR(50) NOT NULL,
+`ClassStatus` TINYTEXT NOT NULL,
+CONSTRAINT `studentenrollment_ibfk_1` FOREIGN KEY (`Username`) REFERENCES `UserAccounts`(`Username`) ON DELETE CASCADE,
+CONSTRAINT `studentenrollment_ibfk_2` FOREIGN KEY (`ClassCode`) REFERENCES `ClassRoster`(`ClassCode`) ON DELETE CASCADE,
+CONSTRAINT `studentenrollment_ibfk_3` FOREIGN KEY (`ClassName`) REFERENCES `ClassRoster`(`ClassName`) ON DELETE CASCADE
+)ENGINE=INNODB AUTO_INCREMENT=1;
+
