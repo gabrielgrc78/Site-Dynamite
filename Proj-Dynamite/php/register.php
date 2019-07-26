d910980cf7ef3dce4ae6a693dcbb6e997b511e2c
Marcus Salas <marcussalas2@gmail.com>
2019-07-26 14:52:51 -0400

    fix

--- Proj-Dynamite/php/register.php
+++ Proj-Dynamite/php/register.php
@@ -32,22 +32,4 @@
   </form>
 </div>
 </div>
-=======
-  <div class="rectangle">
-    <form action="php/include/doregister.php" method="post" >
-      <label for="email">Email</label>
-      <input type="text" id="email" name="mail" placeholder="email" required>
-
-      <label for="password">Password</label>
-      <input type="password" id="password" name="pwd" placeholder="password" required>
-
-      <label for="repeat password">Repeat Password</label>
-      <input type="password" id="pwd-repeat" name="pwd-repeat" placeholder="repeat password">
-
-      <button type="submit" name="signup-submit">Sign up</button>
-    </form>
-  </div>
-
-
-</body>
 </html>
